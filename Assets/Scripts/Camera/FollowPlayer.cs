768a2ea81a1c812965691c081d1e447a9ec954b6
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-11-11 16:58:30 +0100

    added models and start camera and enemy

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private float cameraSpeed;
+    private GameObject player;
+    private Vector3 velocity = Vector3.zero;
+    
+    void Start()
+    {
+        player = GameObject.Find("PlayerCamPos");
+    }
+
+    void Update()
+    {
+        MoveToPlayer();
+    }
+
+    private void MoveToPlayer()
+    {
+        float step = cameraSpeed * Time.deltaTime;
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position,ref velocity ,step);
+    }
+}
