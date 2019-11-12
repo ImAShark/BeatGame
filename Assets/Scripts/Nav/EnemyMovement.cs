908aa9c05fc70c42f1ea09ae8b75013d2e85ff25
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-11-12 11:35:08 +0100

    added nav mesh

---
+++ Assets/Scripts/Nav/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private GameObject Player;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        agent.SetDestination(Player.transform.position);
+    }
+
+    private void CreatePath()
+    {
+        NavMeshPath path = new NavMeshPath();
+        agent.CalculatePath(Player.transform.position,path);
+    }
+}
