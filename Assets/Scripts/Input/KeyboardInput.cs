9ef4c4cb110506c09d2479b36761c739c8d764d6
JudaHensen <24311@ma-web.nl>
2019-11-19 15:42:15 +0100

    Finished 8-directional player movement.

--- Assets/Scripts/Input/KeyboardInput.cs
+++ Assets/Scripts/Input/KeyboardInput.cs
@@ -32,18 +32,18 @@ public class KeyboardInput : BaseInput
         if (Input.GetKey(MoveLeft)) { direction.x --; }
         if (Input.GetKey(MoveRight)) { direction.x ++; }
 
-        if (direction.x == 0 && direction.y == 1) { SendMovement(InputHandler.Directions.North); }
-        else if (direction.x == 1  && direction.y == 1 ) { SendMovement(InputHandler.Directions.NorthEast); }
-        else if (direction.x == 1  && direction.y == 0 ) { SendMovement(InputHandler.Directions.East); }
+        if      (direction.x == 0  && direction.y ==  1) { SendMovement(InputHandler.Directions.North); }
+        else if (direction.x == 1  && direction.y ==  1) { SendMovement(InputHandler.Directions.NorthEast); }
+        else if (direction.x == 1  && direction.y ==  0) { SendMovement(InputHandler.Directions.East); }
         else if (direction.x == 1  && direction.y == -1) { SendMovement(InputHandler.Directions.SouthEast); }
         else if (direction.x == 0  && direction.y == -1) { SendMovement(InputHandler.Directions.South); }
         else if (direction.x == -1 && direction.y == -1) { SendMovement(InputHandler.Directions.SouthWest); }
-        else if (direction.x == -1 && direction.y == 0 ) { SendMovement(InputHandler.Directions.West); }
-        else if (direction.x == -1 && direction.y == 1 ) { SendMovement(InputHandler.Directions.NorthWest); }
+        else if (direction.x == -1 && direction.y ==  0) { SendMovement(InputHandler.Directions.West); }
+        else if (direction.x == -1 && direction.y ==  1) { SendMovement(InputHandler.Directions.NorthWest); }
 
         // OneShots
         if (Input.GetKey(Jump)) { SendOneShot(InputHandler.OneShots.Jump); }
-        if (Input.GetKey(Interact)) { SendOneShot(InputHandler.OneShots.Interact);  }
+        if (Input.GetKey(Interact)) { SendOneShot(InputHandler.OneShots.Interact); }
         if (Input.GetMouseButtonDown(0)) { SendOneShot(InputHandler.OneShots.Attack); }
 
         // Aim
